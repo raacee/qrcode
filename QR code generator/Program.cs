cd329572eb6aad64b555851c22bc008d3dcb1ae9
raacee <raceldamechli@outlook.fr>
2022-04-24 16:24:33 +0200

    Start of the project - Creating classes

---
+++ QR code generator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Projet_Info;
+
+namespace QR_code_generator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Module : Pixel
+    {
+        private bool value;
+        public Module(bool p)
+        {
+            this.value = p;
+            try
+            {
+                this.Red = Convert.ToByte(Convert.ToByte(p) * 255);
+                this.Green = Convert.ToByte(Convert.ToByte(p) * 255);
+                this.Blue = Convert.ToByte(Convert.ToByte(p) * 255);
+            }
+            //should never happen
+            catch (OverflowException)
+            {
+                Console.WriteLine("Pixel color byte conversion resulted in a number over 255");
+            }
+            
+        }
+    }
+
+    public class ColoredException : Exception
+    {
+        private new string Message = "Module is colored, check pixels value";
+    }
+}
