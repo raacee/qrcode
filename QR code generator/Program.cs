635906513d2969a35e5b12a75015b6d0fae2042b
raacee <raceldamechli@outlook.fr>
2022-04-25 11:56:28 +0200

    Progress

--- QR code generator/Program.cs
+++ QR code generator/Program.cs
@@ -1,38 +1,105 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using Projet_Info;
+using System.Diagnostics;
+using System.Xml.Serialization;
 
 namespace QR_code_generator
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            
+            QRCode test = new QRCode("Hello World");
+            test.Generate();
         }
     }
 
-    public class Module : Pixel
+    internal class QRCode
     {
-        private bool value;
+        private byte version = 1;
+        private bool[] mode = {false,false,true,false};
+        private bool[] length;
+        private bool[] information;
+        private bool[] data;
+        private bool[] allbytes;
+        
+        public QRCode(string text)
+        {
+            
+        }
+        
+        //generate the image
+        public void Generate()
+        {
+            
+        }
+
+        public static string BoolArrayToBinString(bool[] arr)
+        {
+            string res = "";
+            foreach (var v in arr)
+            {
+                res += Convert.ToChar(Convert.ToByte(v));
+            }
+            return res;
+        }
+    }
+
+    internal class Module
+    { 
+        public readonly bool value;
+        public readonly bool skippable;
+        public readonly byte red;
+        public readonly byte green;
+        public readonly byte blue;
         public Module(bool p)
         {
             this.value = p;
             try
             {
-                this.Red = Convert.ToByte(Convert.ToByte(p) * 255);
-                this.Green = Convert.ToByte(Convert.ToByte(p) * 255);
-                this.Blue = Convert.ToByte(Convert.ToByte(p) * 255);
+                this.red = Convert.ToByte(255-Convert.ToByte(p) * 255);
+                this.green = Convert.ToByte(255-Convert.ToByte(p) * 255);
+                this.blue = Convert.ToByte(255-Convert.ToByte(p) * 255);
             }
+            
             //should never happen
             catch (OverflowException)
             {
                 Console.WriteLine("Pixel color byte conversion resulted in a number over 255");
             }
+        }
+        public Module(int p)
+        {
+            this.value = Convert.ToBoolean(p);
+            try
+            {
+                this.red = Convert.ToByte(255-Convert.ToByte(p) * 255);
+                this.green = Convert.ToByte(255-Convert.ToByte(p) * 255);
+                this.blue = Convert.ToByte(255-Convert.ToByte(p) * 255);
+            }
             
+            //should never happen
+            catch (OverflowException)
+            {
+                Console.WriteLine("Pixel color byte conversion resulted in a number over 255");
+            }
+        }
+        public Module(byte p)
+        {
+            this.value = Convert.ToBoolean(p);
+            try
+            {
+                this.red = Convert.ToByte(255-p * 255);
+                this.green = Convert.ToByte(255-p * 255);
+                this.blue = Convert.ToByte(255-p * 255);
+            }
+            
+            //should never happen
+            catch (OverflowException)
+            {
+                Console.WriteLine("Pixel color byte conversion resulted in a number over 255");
+            }
         }
     }
-
     public class ColoredException : Exception
     {
         private new string Message = "Module is colored, check pixels value";
